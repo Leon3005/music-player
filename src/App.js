fdd34b9a3e09cd985298a5d9d136894ad4eccd68
Leon <leonwheeler08@gmail.com>
2021-05-15 08:09:54 +0100

    Created a state to see the songs on the App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 //Import styles
 import "./styles/app.scss";
 //Adding components
 import Player from "./components/Player";
 import Song from "./components/Song";
+//Importing data
+import data from "./util";
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
   return (
     <div className="App">
       <Song />
