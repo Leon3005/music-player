b1cc8222a24748241d7d547bd6196434f647bf6a
Leon <leonwheeler08@gmail.com>
2021-05-20 07:18:21 +0100

    Added styling/positioning for library UI

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -4,8 +4,10 @@ const LibrarySong = ({ song }) => {
   return (
     <div className="librarySong">
       <img src={song.cover} alt="song cover"></img>
-      <h3>{song.name}</h3>
-      <h4>{song.artist}</h4>
+      <div className="songDescription">
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
     </div>
   );
 };
