cace7f0ecc38f80fdb78744c93f5e15040740939
Leon <leonwheeler08@gmail.com>
2021-05-23 21:52:44 +0100

    Currently playing song now changes colour

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -7,10 +7,26 @@ const LibrarySong = ({
   id,
   audioRef,
   isPlaying,
+  setSongs,
 }) => {
   const songSelectHandler = () => {
     const selectedSong = songs.filter((state) => state.id === id);
     setCurrentSong(selectedSong[0]);
+    // Add active song
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
     if (isPlaying) {
       const playPromise = audioRef.current.play();
       if (playPromise !== undefined) {
@@ -21,7 +37,10 @@ const LibrarySong = ({
     }
   };
   return (
-    <div onClick={songSelectHandler} className="librarySong">
+    <div
+      onClick={songSelectHandler}
+      className={`librarySong ${song.active ? "selected" : ""}`}
+    >
       <img src={song.cover} alt="song cover"></img>
       <div className="songDescription">
         <h3>{song.name}</h3>
