599a7e27dab75bedbe8b6aad01113061ecf063cf
Leon <leonwheeler08@gmail.com>
2021-07-26 08:44:33 +0100

    Added mobile styling

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
       <Navbar
         libraryStatus={libraryStatus}
         setLibraryStatus={setLibraryStatus}
