1c09ac805fdbe26952f19d6f476f5fa3bdc54c8d
Leon <leonwheeler08@gmail.com>
2021-07-27 10:53:33 +0100

    Added jqeury to hide modal, now need to use jquery to show modal.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,40 +1,57 @@
 import React from "react";
+import $ from "jquery";
 
 const Modal = () => {
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    const artist = $("#artist").value;
+    const songName = $("#songName").value;
+    const songLink = $("#songLink").value;
+
+    if (artist === "" || songName === "" || songLink === "") {
+      $(".error-text").remove();
+      $(event.target).append(`
+      <p class="error-text"> Please fill all fields. </p>
+      `);
+      return;
+    }
+
+    $(".modal-window").hide();
+  };
+
   return (
-    <div>
-      <div id="open-modal" class="modal-window">
+    <div id="open-modal" class="modal-window">
+      <div>
+        <a href="#" title="Close" class="modal-close">
+          Close
+        </a>
+        <h1>Enter song details:</h1>
         <div>
-          <a href="#" title="Close" class="modal-close">
-            Close
-          </a>
-          <h1>Enter song details:</h1>
-          <div>
-            <form className="requestForm">
-              <input
-                type="text"
-                name="artist"
-                className="form-style"
-                placeholder="Artist"
-                id="artist"
-              />
-              <input
-                type="text"
-                name="songName"
-                className="form-style"
-                placeholder="Song Name"
-                id="songName"
-              />
-              <input
-                type="text"
-                name="songLink"
-                className="form-style"
-                placeholder="Song Youtube Link"
-                id="songLame"
-              />
-              <button type="submit">Request</button>
-            </form>
-          </div>
+          <form className="requestForm" onSubmit={onSubmit}>
+            <input
+              type="text"
+              name="artist"
+              className="form-style"
+              placeholder="Artist"
+              id="artist"
+            />
+            <input
+              type="text"
+              name="songName"
+              className="form-style"
+              placeholder="Song Name"
+              id="songName"
+            />
+            <input
+              type="text"
+              name="songLink"
+              className="form-style"
+              placeholder="Song Youtube Link"
+              id="songLink"
+            />
+            <button type="submit">Request</button>
+          </form>
         </div>
       </div>
     </div>
